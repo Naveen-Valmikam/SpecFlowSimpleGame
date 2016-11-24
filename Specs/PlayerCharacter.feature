352580be2b4c08c29c60a7cce5b7b07ad951f60e
Naveen-Valmikam <naveen.valmikam@gmail.com>
2016-11-25 10:00:19 +1100

    Specflow - basic scenarios

---
+++ Specs/PlayerCharacter.feature
@@ -0,0 +1,20 @@
+﻿Feature: PlayerCharacter
+	In order to play the game
+	As a human player
+	I want my character attributes to be correctly represented
+
+
+Scenario: Taking no damage when hit doesn't affect health
+	Given I'm a new player
+	When I take 0 damage
+	Then My health should now be 100
+
+Scenario: Starting health is reduced when player takes a hit
+Given I'm a new player
+When I take 30 damage
+Then My health should now be 70
+
+Scenario: Taking too much damageresults in player death
+Given I'm a new player
+When I take 100 damage
+Then I am dead
---
+++ Specs/PlayerCharacterSteps.cs
@@ -0,0 +1,55 @@
+﻿using GameCore;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Specs
+{
+    [Binding]
+    public class PlayerCharacterSteps
+    {
+        PlayerCharacter _player;
+
+        [Given(@"I'm a new player")]
+        public void GivenImANewPlayer()
+        {
+            _player = new PlayerCharacter();
+        }
+        
+        [When(@"I take 0 damage")]
+        public void WhenITake0Damage()
+        {
+            _player.Hit(0);
+        }
+        
+        [Then(@"My health should now be 100")]
+        public void ThenMyHealthShouldNowBe100()
+        {
+            Assert.Equal(100, _player.Health);
+        }
+
+
+        [When(@"I take 30 damage")]
+        public void WhenITake30Damage()
+        {
+            _player.Hit(30);
+        }
+
+        [Then(@"My health should now be 70")]
+        public void ThenMyHealthShouldNowBe70()
+        {
+            Assert.Equal(70, _player.Health);
+        }
+
+        [When(@"I take 100 damage")]
+        public void WhenITake100Damage()
+        {
+            _player.Hit(100);
+        }
+
+        [Then(@"I am dead")]
+        public void ThenIAmDead()
+        {
+            Assert.Equal(true, _player.IsDead);
+        }
+    }
+}
